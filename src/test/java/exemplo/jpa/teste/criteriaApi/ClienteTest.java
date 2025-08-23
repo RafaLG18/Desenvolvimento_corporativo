161c941049762b84ba86b158927a0cca62c39dc7
rafael.leal <rafaellealg18@gmail.com>
2025-08-22 21:23:53 -0300

    Adicionando CriteriaAPi teste em cliente

--- src/test/java/exemplo/jpa/teste/criteriaApi/ClienteTest.java
+++ src/test/java/exemplo/jpa/teste/criteriaApi/ClienteTest.java
@@ -4,10 +4,43 @@
  */
 package exemplo.jpa.teste.criteriaApi;
 
+import exemplo.jpa.Cliente;
+import exemplo.jpa.teste.Teste;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+import org.eclipse.persistence.sessions.Session;
+import static org.junit.Assert.assertEquals;
+//import org.junit.jupiter.api.BeforeEach
+import org.junit.Before;
+import org.junit.Test;
 /**
  *
  * @author rafael
  */
-public class ClienteTest {
+public class ClienteTest extends Teste {
+    
+    CriteriaBuilder cb=null;
+    Root<Cliente> root=null;
+    CriteriaQuery<Cliente> cq=null;
+    
+    @Before
+    public void setup(){
+        cb = em.getCriteriaBuilder();
+        cq=cb.createQuery(Cliente.class);
+        root=cq.from(Cliente.class);        
+    }
+    @Test
+    public void QuantidadeDeClientes(){
+        cq.select(root);
+        TypedQuery<Cliente> tq=em.createQuery(cq);
+        List<Cliente> clientes = tq.getResultList();
+        
+        assertEquals(11,clientes.size());
+    }  
+    
+    
     
 }
